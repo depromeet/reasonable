482692a9202ebf908f67c0f1f95a6066210a794f
Goodnews <krgoodnews@gmail.com>
2017-04-22 14:49:40 +0900

    Create Project / Alamofire, SwiftyJSON 설

---
+++ iOS/Reasonable/Reasonable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reasonable
+//
+//  Created by Goodnews on 2017. 4. 22..
+//  Copyright © 2017년 Depromeet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOS/Reasonable/ReasonableTests/ReasonableTests.swift
@@ -0,0 +1,36 @@
+//
+//  ReasonableTests.swift
+//  ReasonableTests
+//
+//  Created by Goodnews on 2017. 4. 22..
+//  Copyright © 2017년 Depromeet. All rights reserved.
+//
+
+import XCTest
+@testable import Reasonable
+
+class ReasonableTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
