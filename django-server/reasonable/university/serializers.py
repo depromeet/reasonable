abbee0f4584e21ad0e69465a1be634c2393b9069
Jimmy Kim <jkimab@connect.ust.hk>
2017-04-26 09:15:59 +0900

    chore: added comment for question

--- django-server/reasonable/university/serializers.py
+++ django-server/reasonable/university/serializers.py
@@ -12,5 +12,6 @@ class UniversitySerializer(serializers.ModelSerializer):
 class UniversityListSerializer(serializers.ModelSerializer):
     class Meta:
         model = University
+        # class 안에 def 를 넣어야하는거같은데 왜 넣는거지 모르겠음 시간부족해서 못봄... 그냥 일단 만듬
         # fields = ['id', 'name', 'num_pages', 'naver_link', 'image_link']
         fields = '__all__'
