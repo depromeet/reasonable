00596bb1009d4c8ceb59e88b89047d0664858e6e
JeongTaek Lim <ljt1100@gmail.com>
2017-04-30 07:24:22 +0900

    server ip change

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 
-export const SERVER_ADDRESS = '54.218.197.65';
+// export const SERVER_ADDRESS = '54.218.197.65';
+export const SERVER_ADDRESS = 'localhost:8000';
 export const UTC_TIME_ZONE = '+0900';
